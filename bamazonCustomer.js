8fb635fef24cdd39684361c83f85e5675ccb8fc0
Leonard Ezeudu <leonardezeudu@gmail.com>
2019-08-31 19:48:47 -0400

    added bamazonManager.js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,12 @@
+// connecting inquirer package
+
 const inquirer = require("inquirer");
 
+// connecting mysql package
+
 const mysql = require("mysql");
+
+// declaring variables needed
 
 let itemId;
 
@@ -8,51 +14,85 @@ let itemUnit;
 
 let prize;
 
+
+
+
+// connecting to bamazon database
+
 const connection = mysql.createConnection({
+
   host: "localhost",
 
   // Your port; if not 3306
+
   port: 3306,
 
   // Your username
+
   user: "root",
 
   // Your password
+
   password: "leo464315",
+
   database: "bamazon"
 });
 
 connection.connect(function(err) {
+
   if (err) throw err;
+
   console.log("connected as id " + connection.threadId);
+
+  // after connection to bamazon database, display products to customer
+
   displayProducts();
   
 });
 
+
+
+
+// function that displays available products with item id and prices to the customer
+
 function displayProducts() {
+
+    // selects every item in the database
+
     connection.query("SELECT * FROM products", function(err, res) {
+
         if (err) throw err;
 
-        
+        // loop through response array and display item ids, item names and prices        
 
         for (let i = 0;i < res.length; i++) {
             
           console.log(`
             ####################################################
-                Item id: ${res[i].item_id}
-                Item Name: ${res[i].product_name}
-                Item Price: $${res[i].price}
+            Item id: ${res[i].item_id}
+            Item Name: ${res[i].product_name}
+            item Department: ${res[i].department_name}
+            Item Price: $${res[i].price}
+            Item Quantity: ${res[i].stock_quantity}
             ####################################################`);
                        
            
         
 
          }
+
+         // after displaying products to customers, use inquirer to ask them what they want
+
         getInquirer();
     });
 }
 
+
+
+// function that asks customers what they want to purchase and collects their reply
+
 async function getInquirer() {
+
     await inquirer.prompt([
         {
             type:"input",
@@ -73,11 +113,15 @@ async function getInquirer() {
           }
     ]).then(function(inquirerResponse) {
 
+        // if customer typed item id and quantity they want, check if it's in stock
+
         if((inquirerResponse.id) && (inquirerResponse.quantity)) {
 
            itemId = inquirerResponse.id;
 
            itemUnit = inquirerResponse.quantity;
+
+           // item id and quantity passed in as inputs to check if item is available
            
            availability(itemId,itemUnit);
 
@@ -86,46 +130,72 @@ async function getInquirer() {
 }
 
 
+
+
+// function that checks if the customer's item is in stock
+
 function availability(itemId,itemUnit) {
+
+    // using the item id, check if the item is available
 
     connection.query("SELECT * FROM products WHERE ?",
     {
         item_id: itemId
     }, 
     function(err, res) {
+
         if (err) throw err;
 
+         // assign response to a variable "order"
+
          let order = res[0];
-
          
-
+         // if item is available
 
         if (order.stock_quantity >= itemUnit) {
+
+            // sell to customer and update stock
             
             prize = order.price;
-            let newId = order.item_id;
-            let oldStock = order.stock_quantity;
-            let newStock = oldStock - itemUnit;
-            updateStock(newId,newStock);
-            
 
+            let newId = order.item_id;
+
+            let oldStock = order.stock_quantity;
+
+            let newStock = oldStock - itemUnit;
+
+            // update stock with the new quantity after recent sales
+
+            updateStock(newId,newStock);   
+            
+            // if item not in stock
 
         }else if(order.stock_quantity < itemUnit) {
+
+            // inform the customer the item is out of stock
+
             console.log(`
             ####################################################
-            Sorry We don't have it in stock at the moment
+            Sorry, this item is not in stock at the moment.
             ####################################################`);
+
             connection.end();
         }
         
 
     });
+    
     return prize, itemUnit;
 }
 
 
+
+// function that updates stock after each sale
+
 function updateStock(newId,newStock) {
+
     connection.query(
+
         "UPDATE products SET ? WHERE ?",
         [
           {
@@ -136,7 +206,9 @@ function updateStock(newId,newStock) {
           }
         ],
         function(err, res) {
+
           if (err) throw err;
+
             console.log(`
             ####################################################
                           UPDATING PRODUCT'S TABLE
@@ -144,7 +216,8 @@ function updateStock(newId,newStock) {
             product with item id ${newId} has been updated to 
             ${newStock} quantity in stock
             ####################################################`);
-          
+
+            // after stock is update, display customer's receipt          
           
             displayInvoice(prize,itemUnit);
   
@@ -155,7 +228,13 @@ function updateStock(newId,newStock) {
       );
 }
 
+
+
+
+// function that displays customer's receipt
+
 function displayInvoice(prize,itemUnit) {
+
     console.log(`
             ####################################################
                                   RECEIPT
---
+++ bamazonManager.js
@@ -0,0 +1,347 @@
+// connecting inquirer package
+
+const inquirer = require("inquirer");
+
+// connecting mysql package
+
+const mysql = require("mysql");
+
+let stock;
+
+let itemId;
+
+let itemUnit;
+
+let newStock = 0;
+
+
+// connecting to bamazon database
+
+const connection = mysql.createConnection({
+
+    host: "localhost",
+  
+    // Your port; if not 3306
+  
+    port: 3306,
+  
+    // Your username
+  
+    user: "root",
+  
+    // Your password
+  
+    password: "leo464315",
+  
+    database: "bamazon"
+});
+  
+connection.connect(function(err) {
+  
+    if (err) throw err;
+  
+    console.log("connected as id " + connection.threadId);
+  
+    // after connection to bamazon database, display options to manager
+  
+    displayOptions();
+    
+});
+
+
+async function displayOptions() {
+
+    await inquirer.prompt([
+        {
+            type:"list",
+            message:"Good day Manager, What do you want to do?",
+            choices:["View Products for Sale","View Low Inventory","Add to Inventory","Add New Product"],
+            name: "item"
+        }
+    ]).then(function(inquirerResponse) {
+
+       if(inquirerResponse.item) {
+
+          let task = inquirerResponse.item;
+       
+            if(task === "View Products for Sale") {
+
+               displayProducts();
+
+            }else if(task === "View Low Inventory") {
+
+              lowInventory();
+
+            }else if(task === "Add to Inventory") {
+
+              addInventory();
+
+            }else if(task === "Add New Product") {
+
+              addProduct();
+
+            }
+        }
+    })
+
+}
+
+
+function displayProducts() {
+
+    // selects every item in the database
+
+    connection.query("SELECT * FROM products", function(err, res) {
+
+        if (err) throw err;
+
+        // loop through response array and display item ids, item names and prices        
+
+        for (let i = 0;i < res.length; i++) {
+            
+          console.log(`
+            ####################################################
+                Item id: ${res[i].item_id}
+                Item Name: ${res[i].product_name}
+                item Department: ${res[i].department_name}
+                Item Price: $${res[i].price}
+                Item Quantity: ${res[i].stock_quantity}
+            ####################################################`);                     
+                
+
+        }
+
+        connection.end();
+
+    });
+}
+
+
+
+function lowInventory() {
+
+    // selects every item in the database
+
+    connection.query("SELECT * FROM products", function(err, res) {
+
+        if (err) throw err;
+
+        // loop through response array and display item ids, item names and prices        
+
+        for (let i = 0;i < res.length; i++) {
+
+            stock = res[i].stock_quantity;
+
+            if(stock < 5) {
+                       
+             console.log(`
+             ##################################################
+             Item id: ${res[i].item_id}
+             Item Name: ${res[i].product_name}
+             Item Price: $${res[i].price}
+             Item Quantity: ${res[i].stock_quantity}
+             ##################################################`);
+
+             connection,end();            
+            }                         
+
+        }
+
+            console.log(`
+            ###################################################
+            We dont have any low inventory at the moment
+            check later
+            ###################################################`)
+
+        connection.end();
+
+    });
+
+}
+
+
+
+async function addInventory() {
+    await inquirer.prompt([
+        {
+            type:"input",
+            message:`
+            ####################################################                            
+                                Hey Manager
+            ____________________________________________________
+            What's the item id of the item you want to add?
+            ####################################################`,
+            name: "id"
+        },{
+            type:"input",
+            message:`
+            ####################################################
+            How many do you want to add?
+            ####################################################`,
+            name:"quantity"
+          }
+    ]).then(function(inquirerResponse) {
+
+        // if customer typed item id and quantity they want, check if it's in stock
+
+        if((inquirerResponse.id) && (inquirerResponse.quantity)) {
+
+           itemId = inquirerResponse.id;
+
+           itemUnit = parseInt(inquirerResponse.quantity);
+
+           // item id and quantity passed in as inputs to check if item is available
+           
+           itemSelect(itemId,itemUnit);
+
+        }
+    })
+}
+
+
+
+
+function itemSelect(itemId,itemUnit) {
+    connection.query("SELECT * FROM products WHERE ?",
+    {
+        item_id: itemId
+    }, 
+    function(err, res) {
+
+        if (err) throw err;       
+                 
+            newStock = res[0].stock_quantity + itemUnit;
+
+
+            // update stock with the new quantity after recent sales
+
+            updateStock(itemId,newStock);   
+            
+            // if item not in stock
+
+
+    });
+}
+
+
+
+function updateStock(itemId,newStock) {
+
+    connection.query(
+
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: newStock
+          },
+          {
+            item_id: itemId
+          }
+        ],
+        function(err, res) {
+
+          if (err) throw err;
+
+            console.log(`
+            ####################################################
+                          UPDATING PRODUCT'S TABLE
+            ____________________________________________________              
+            product with item id ${itemId} has been updated to 
+            ${newStock} quantity in stock
+            ####################################################`);              
+                     
+  
+            connection.end();
+          }
+  
+          
+      );
+}
+
+
+
+async function addProduct() {
+
+    await inquirer.prompt([
+        {
+            type:"input",
+            message:`
+            ####################################################                            
+                                Hey Manager
+            ____________________________________________________
+            What's the name of the new item you want to add?
+            ####################################################`,
+            name: "name"
+        },{
+            type:"input",
+            message:`
+            ####################################################
+            Which department does the new item belong?
+            ####################################################`,
+            name:"department"
+          },{
+            type:"input",
+            message:`
+            ####################################################
+            What is the price of the new item?
+            ####################################################`,
+            name:"prize"
+          },{
+            type:"input",
+            message:`
+            ####################################################
+            How many of the new item are you adding today?
+            ####################################################`,
+            name:"quantity"
+          }
+    ]).then(function(inquirerResponse) {
+
+        // if customer typed item id and quantity they want, check if it's in stock
+
+        
+
+           let newItem = inquirerResponse.name;
+
+           let newDepartment = inquirerResponse.department;
+
+           let newPrize = inquirerResponse.prize;
+
+           let newQuantity = parseInt(inquirerResponse.quantity);
+
+           // item id and quantity passed in as inputs to check if item is available
+           
+           newProduct(newItem,newDepartment,newPrize,newQuantity);
+
+        
+    })
+}
+
+
+
+function newProduct(newItem,newDepartment,newPrize,newQuantity) {
+
+    connection.query(
+        "INSERT INTO products SET ?",
+        [
+          {
+            product_name: newItem,
+            department_name: newDepartment,
+            price: newPrize,
+            stock_quantity: newQuantity
+          }
+        ],
+        function(err, res) {
+          if (err) throw err;            
+            
+            console.log(`
+            ####################################################
+                          UPDATING PRODUCT'S TABLE
+            ____________________________________________________              
+            ${newItem} has been added to the products table
+            ####################################################`);
+
+            connection.end();
+            
+        }
+    );
+}
